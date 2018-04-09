058665a9211b6dc7b2e784d781ab7697842321ab
David P. Bauer <dbauer1@udayton.edu>
2018-04-09 09:58:12 -0400

    TSUGI-36 Initial commit

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+// The SQL to uninstall this tool
+$DATABASE_UNINSTALL = array(
+    // Nothing upon uninstall
+);
+
+// The SQL to create the tables if they don't exist
+$DATABASE_INSTALL = array(
+    // Database stuff goes here
+);
---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+
+$REGISTER_LTI2 = array(
+    "name" => "Pre/Post", // Name of the tool
+    "FontAwesome" => "fa-file-text", // Icon for the tool
+    "short_name" => "Pre/Post",
+    "description" => "This is a simple tool to allow an instructor to create a question for a student to answer before and after an activity.", // Tool description
+    "messages" => array("launch", "launch_grade")
+);
