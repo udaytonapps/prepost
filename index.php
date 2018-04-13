5cf1b23ae0e61ff988b5833b4e28b7934d071b1c
James W <washingtonj3@udayton.edu>
2018-04-13 11:17:11 -0400

    TSUGI-37 Main instructor/user base function Commit On load the tool checks if the user is a student of an instructor Then checks if the page has been set up and if there is a question set up FOR TESTING: Makes a test question on instructor page if there is no question Deletes the test question if there is a question.

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
 require_once "../config.php";
+require_once "dao/PP_DAO.php";
 
 use \Tsugi\Core\LTIX;
+use \PP\DAO\PP_DAO;
 
 // Retrieve the launch data if present
 $LTI = LTIX::requireData();
 
 $p = $CFG->dbprefix;
 
-$IV_DAO = new IV_DAO($PDOX, $p);
+$PP_DAO = new PP_DAO($PDOX, $p);
 
 // Start of the output
 $OUTPUT->header();
@@ -17,7 +19,48 @@ $OUTPUT->header();
     <!-- Our main css file that overrides default Tsugi styling -->
     <link rel="stylesheet" type="text/css" href="styles/main.css">
 <?php
+
 $OUTPUT->bodyStart();
+$main_Id = $PP_DAO->getMainId($CONTEXT->id, $LINK->id);
+if ($USER->instructor) {
+    if (!$main_Id) {
+        $_SESSION["main_ID"] = $PP_DAO->createMain($USER->id, $CONTEXT->id, $LINK->id);
+    } else {
+        $_SESSION["main_ID"] = $main_Id;
+    }
+    $question_ID = $PP_DAO->getQuestionId($_SESSION["main_ID"]);
+    if (!$question_ID) {
+        ?>
+        <div class="text-center">
+            <h3>Here you can add a question for this tool. Testing will do it automatically. Creating test question</h3>
+        </div>
+        <?php
+        $Question = "!!!!";
+        $PP_DAO->createQuestion($_SESSION["main_ID"], $Question);
+    } else {
+        ?>
+        <div class="text-center">
+            <h3>Test question goes here</h3>
+            <h3>For testing purposes Deleting test question</h3>
+        </div>
+        <?php
+        $PP_DAO->deleteQuestion($_SESSION["main_ID"]);
+    }
+} else {
+    if (!$main_Id) {
+        ?>
+        <div class="text-center">
+            <h3>This page has not been set up yet</h3>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div class="text-center">
+            <h3>Default info</h3>
+        </div>
+        <?php
+    }
+}
 
 $OUTPUT->footerStart();
 ?>
